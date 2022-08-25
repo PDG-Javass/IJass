74de48bd2896d986869061cabe4a096bba89c10f
Hugo Huart <71184949+HUART1848@users.noreply.github.com>
2022-08-25 15:05:46 +0200

    [FEATURE] First communication infrastructure (#16)
    * feat: First comm infrastructure
    
    * fix: Fix formatting

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  const URL = "https://server-xxcwjwt7qq-ue.a.run.app/coucou";
+
+  async function getData() {
+    const res = await fetch(URL);
+    const text = await res.text();
+
+    if (res.ok) {
+      return text;
+    } else {
+      throw new Error(text);
+    }
+  }
+
+  let data;
+
+	function handleClick() {
+		data = getData();
+	}
 </script>
 
 <main>
-  <h1>Coucou salut</h1>
-  <Counter />
+  <h1>Bienvenue</h1>
+  <h2>Voici les dernières données</h2>
+
+  {#if data != undefined}
+    {#await data}
+      <p>...veuillez patienter</p>
+    {:then content}
+      <p>Le serveur dit {data}</p>
+    {:catch error}
+      <p style="color: red">Erreur</p>
+    {/await}
+  {/if}
+  <button on:click={handleClick}>Obtenir des données</button>
 </main>
 
 <style>
--- server/src/main/java/ch/ijass/server/Mappings.java
+++ server/src/main/java/ch/ijass/server/Mappings.java
@@ -1,9 +1,11 @@
 package ch.ijass.server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = Utils.expectedClientUrl)
 public class Mappings {
   @GetMapping("/coucou")
   public String coucou() {
