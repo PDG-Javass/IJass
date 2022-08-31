3b07334303f8cd489e8c5f8307e43272403c6c09
Hugo Huart <hugo.huart@heig-vd.ch>
2022-08-31 13:50:59 +0200

    feat: Initial homepage

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+  import Board from "./lib/Board.svelte";
+  import Home from "./lib/Home.svelte";
+  import Rules from "./lib/Rules.svelte";
 
-import Board from "./lib/Board.svelte"
+  const pages = [
+    { name: "board", component: Board },
+    { name: "home", component: Home },
+    { name: "rules", component: Rules },
+  ];
+
+  let selected = pages[1];
 </script>
 
 <main>
-  <Board/>
+  <svelte:component this={selected.component} />
 </main>
+
+<style>
+</style>
---
+++ client/src/lib/Home.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import homeCard from "../assets/homeCards.webp";
+
+    function handleStart() {}
+</script>
+
+<main>
+    <h1>Bienvenue sur Javass</h1>
+    <img src={homeCard} alt="Cartes de Jass" />
+
+    <button on:click={handleStart}>Commencer une partie</button>
+</main>
+
+<style>
+    h1 {
+        font-size: 5em;
+    }
+
+    img {
+        display: block;
+        margin: auto;
+        width: 50%;
+    }
+
+    button {
+        border-radius: 1em;
+        padding: 2rem;
+        background-color: lightsalmon;
+        font-weight: bold;
+        margin-top: 1em;
+        margin-bottom: 1em;
+        max-width: 80%;
+        font-size: 3em;
+        text-align: center;
+    }
+
+    button:hover {
+        background-color: lightcoral;
+        transition: background-color 0.5s ease;
+    }
+</style>
