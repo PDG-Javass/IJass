38b31f5ee8e5512e68a66c88e8b4790821862e99
Hugo Huart <71184949+HUART1848@users.noreply.github.com>
2022-08-31 16:18:18 +0200

    [FEATURE] Home page (#33)
    * feat: add cardboard
    
    * feat: les cartes vont au centre et disparaissent
    
    * docs: ajouter les commentaires
    
    * feat: nouvelle architecture
    
    * renommer les cartes selon le backend
    
    * feat: Initial homepage
    
    Co-authored-by: maelle <maelle.vogel@heig-vd.ch>
    Co-authored-by: Maëlle.V <71427372+vogelma@users.noreply.github.com>

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,38 +1,19 @@
 <script lang="ts">
-  const URL = "https://server-xxcwjwt7qq-ue.a.run.app/coucou";
+  import Board from "./lib/Board.svelte";
+  import Home from "./lib/Home.svelte";
+  import Rules from "./lib/Rules.svelte";
 
-  async function getData() {
-    const res = await fetch(URL);
-    const text = await res.text();
+  const pages = [
+    { name: "board", component: Board },
+    { name: "home", component: Home },
+    { name: "rules", component: Rules },
+  ];
 
-    if (res.ok) {
-      return text;
-    } else {
-      throw new Error(text);
-    }
-  }
-
-  let data;
-
-	function handleClick() {
-		data = getData();
-	}
+  let selected = pages[1];
 </script>
 
 <main>
-  <h1>Bienvenue</h1>
-  <h2>Voici les dernières données</h2>
-
-  {#if data != undefined}
-    {#await data}
-      <p>...veuillez patienter</p>
-    {:then content}
-      <p>Le serveur dit {content}</p>
-    {:catch error}
-      <p style="color: red">Erreur</p>
-    {/await}
-  {/if}
-  <button on:click={handleClick}>Obtenir des données</button>
+  <svelte:component this={selected.component} />
 </main>
 
 <style>
---
+++ client/src/lib/Board.svelte
@@ -0,0 +1,142 @@
+<script lang="ts">
+  export let selected;
+  //player's deck
+  let deck = [
+    { name: "cards/card_0_0_160.png", visible: true },
+    { name: "cards/card_0_1_160.png", visible: true },
+    { name: "cards/card_0_2_160.png", visible: true },
+    { name: "cards/card_0_3_160.png", visible: true },
+    { name: "cards/card_0_4_160.png", visible: true },
+    { name: "cards/card_0_5_160.png", visible: true },
+    { name: "cards/card_0_6_160.png", visible: true },
+    { name: "cards/card_0_7_160.png", visible: true },
+    { name: "cards/card_0_8_160.png", visible: true },
+  ];
+
+  let card_board = "";
+  let visible = false;
+
+  //play the selected card
+  function moveCardToBoard(x) {
+    deck[x].visible = false;
+    visible = true;
+    card_board = deck[x].name;
+  }
+</script>
+
+<body>
+  <!-- board with cards -->
+  <div class="main">
+    <div class="table">
+      <div class="board">
+        <!-- fold cards -->
+        <div class="tapis">
+          <table class="tab_tapis">
+            <tr>
+              <td />
+              <td id="card_co" class="card-small"
+                ><img src="cards/card_0_0_160.png" alt="carte" /></td
+              >
+              <td />
+            </tr>
+            <tr>
+              <td id="card_op_1" class="card-small"
+                ><img src="cards/card_1_8_160.png" alt="carte" /></td
+              >
+              <td />
+              <td id="card_op_2" class="card-small"
+                ><img src="cards/card_2_7_160.png" alt="carte" /></td
+              >
+            </tr>
+            <tr>
+              <td />
+
+              {#if visible}
+                <td id="card_me" class="card-small"
+                  ><img src={card_board} alt="carte" /></td
+                >
+              {/if}
+
+              <td />
+            </tr>
+          </table>
+        </div>
+
+        <div>
+          <!-- players cards-->
+          <table class="tab_tapis">
+            <tr>
+              <!-- each card is visible at the beginning. On click goes to board and dispear -->
+              {#each deck as { name, visible }, i}
+                {#if visible}
+                  <td id="card_0"
+                    ><div class="card-small">
+                      <img
+                        src={name}
+                        alt="carte"
+                        on:click={() => moveCardToBoard(i)}
+                      />
+                    </div></td
+                  >
+                {/if}
+              {/each}
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+<style>
+  .tapis {
+    margin-bottom: 10%;
+    padding: 10px;
+  }
+
+  .tab_tapis {
+    margin: auto;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    aspect-ratio: 1 / 1.5;
+    border-radius: 15px;
+  }
+
+  .center {
+    text-align: center;
+  }
+
+  .main {
+    width: 1000px;
+    margin: auto;
+    padding: 10px;
+  }
+
+  .table {
+    background-color: green;
+    height: 800px;
+    width: 120%;
+    margin: 0 auto;
+    border: 1em solid black;
+  }
+
+  .board {
+    top: 40%;
+    left: 20%;
+  }
+
+  .card-small {
+    border: 0.2em solid black;
+    border-radius: 10%;
+    height: 150px;
+    width: 105px;
+    /*70% of height*/
+    margin-right: 5px;
+    float: left;
+    background-color: white;
+  }
+</style>
