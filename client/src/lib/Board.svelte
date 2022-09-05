b322691d3cd54608f2c7b9f77d7e89b9fd723121
maelle <maelle.vogel@heig-vd.ch>
2022-09-05 09:56:10 +0200

    ajout de variable pour les bots

--- client/src/lib/Board.svelte
+++ client/src/lib/Board.svelte
@@ -36,7 +36,7 @@
       family: 0,
       value: 8
     },
-  ]
+  ];
 
   //player's deck
   let deck = [
@@ -51,11 +51,14 @@
     { name: "", visible: true, playable: false },
   ];
 
+  let deckBot = [
+    { name: "cards/card_2_2_160.png", visible: false},
+    { name: "cards/card_2_3_160.png", visible: false},
+    { name: "cards/card_2_6_160.png", visible: false},
+  ];
+
   let card_board = "";
-  let visible = false;
-  let visible_co = false;
-  let visible_op_1 = false;
-  let visible_op_2 = false;
+  let visible_me = false;
 
   let trump_me = true;
   let trump_current = "";
@@ -64,31 +67,34 @@
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-   async function showCard(begin){
-    for(let i = 0; i < 4; ++i){
-      await sleep(2000);
-      switch(begin){
-        case 0:
-          visible_op_1 = true;
-          break;
-        case 1:
-          visible_co = true;
-          break;
-        case 2:
-          visible_op_2 = true;
-          break;
-        case 3:
-          {for(let i = 0; i < 9; ++i){
-          deck[i].playable = true;
-    }}
-          break;
-      }
-      ++begin;
+//show card, player card become playable
+  async function showCard(begin){
+  for(let i = 0; i < 4; ++i){
+    await sleep(2000);
+    switch(begin){
+      case 0:
+        {for(let i = 0; i < 9; ++i){
+        deck[i].playable = true;
+  }}
+        break;
+      case 1:
+        deckBot[0].visible = true;
+        break;
+      case 2:
+        deckBot[1].visible = true;
+        break;
+      case 3:
+       deckBot[2].visible = true;
+        break;
       
-  }
+    }
+    ++begin;
+    
+}
 
-  }
+}
 
+//set trump if it's player turn
   function setTrump(id){
     trump_me = false;
     trump_current = "trump_" + id + ".png"
@@ -102,26 +108,50 @@
   function moveCardToBoard(x: number) {
  
     deck[x].visible = false;
-    visible = true;
+    visible_me = true;
     card_board = deck[x].name;
 
     for(let i = 0; i < 9; ++i){
       deck[i].playable = false;
     }
     
-    showCard(0);
+    showCard(1);
 
   }
 
+  //show player deck
   function setDeck(cardState) {
     for (let i = 0; i < deck.length; ++i) {
       deck[i].name = `cards/card_${cardState[i].family}_${cardState[i].value}_160.png`;
-      console.log(`cards/card_${cardState[i].family}_${cardState[i].value}_160.png`);
+
+      deck[i].visible = true;
     }
   }
 
 
-  setDeck(data);
+  
+
+  function playGame(){
+  let j = 1;
+  for(let i = 0; i < 8; ++i){
+    
+    if(j == 4){
+      j = 0;
+    }
+
+    
+
+    //await sleep(2000);
+    visible_me = false;
+
+    ++j;
+  
+  }
+
+}
+
+setDeck(data);
+//playGame();
   
 </script>
 
@@ -140,9 +170,9 @@
             <td/>
             {/if}
 
-            {#if visible_co}
+            {#if deckBot[1].visible}
             <td class="card-small"
-              ><img src="cards/card_2_7_160.png" alt="carte" /></td>
+              ><img src={deckBot[1].name} alt="carte" /></td>
             {:else}
               <td class="card-transparent"
                 ><img src="cards/card_transparent.png" alt="carte" /></td>
@@ -157,9 +187,9 @@
           </tr>
 
           <tr>
-            {#if visible_op_1}
+            {#if deckBot[0].visible}
             <td class="card-small"
-              ><img src="cards/card_2_8_160.png" alt="carte" /></td>
+              ><img src={deckBot[0].name} alt="carte" /></td>
             {:else}
               <td class="card-transparent"
                 ><img src="cards/card_transparent.png" alt="carte" /></td>
@@ -167,9 +197,9 @@
 
             <td/>
 
-            {#if visible_op_2}
+            {#if deckBot[2].visible}
             <td class="card-small"
-              ><img src="cards/card_2_3_160.png" alt="carte" /></td>
+              ><img src={deckBot[2].name} alt="carte" /></td>
             {:else}
               <td  class="card-transparent"
                 ><img src="cards/card_transparent.png" alt="carte" /></td>
@@ -183,7 +213,7 @@
             <td/>
             {/if}
 
-            {#if visible}
+            {#if visible_me}
               <td id="card_me" class="card-small"
                 ><img src={card_board} alt="carte" /></td>
             {:else}
