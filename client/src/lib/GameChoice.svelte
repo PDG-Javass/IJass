d68176ad0485a98ae12546fc99586c23d049692e
Hugo Huart <hugo.huart@heig-vd.ch>
2022-09-07 15:48:27 +0200

    feat: Create choice page

---
+++ client/src/lib/GameChoice.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { nextPage, page } from "../stores";
+    ("https://www.swisslos.ch/fr/jass/informations/les-regles-du-jass/bases-du-jass.html");
+
+    function handleNext() {
+        nextPage(page);
+    }
+</script>
+
+<main />
+
+<style>
+</style>
